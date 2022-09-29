2e4cdc67d96096429e5d62303c48f79a4939673f
Romeo Vasil <romeovasil@Romeos-MacBook-Pro.local>
2022-09-29 10:24:59 +0300

    LogIn and Register without forgot

---
+++ App10/Model/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  App10
+//
+//  Created by Romeo Vasil on 09.09.2022.
+//
+
+import Foundation
---
+++ App10/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  App10
+//
+//  Created by Romeo Vasil on 09.09.2022.
+//
+
+import Foundation
